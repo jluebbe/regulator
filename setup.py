59270db2f4506cfba619402c513cfa9f00b8eff5
Jan Luebbe <jluebbe@debian.org>
2018-05-14 21:42:14 +0200

    add travis setup
    Signed-off-by: Jan Luebbe <jluebbe@debian.org>

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import fastentrypoints
-
 from setuptools import setup
 
 setup(
@@ -12,15 +11,29 @@ setup(
     use_scm_version=True,
     url='https://github.com/jluebbe/regulator',
     python_requires='>=3.5',
-    setup_requires=['pytest-runner', 'setuptools_scm'],
-    tests_require=['pytest-mock', ],
+    setup_requires=[
+        'pytest-runner',
+        'setuptools_scm',
+    ],
+    tests_require=[
+        'hypothesis',
+    ],
+    install_requires=[
+        'attrs',
+        'bitstruct',
+        'click',
+        'prettyprinter',
+        'pygobject',
+        'pyyaml',
+        'sortedcontainers',
+    ],
     packages=[
         'regulator',
     ],
     # the following makes a plugin available to pytest
     entry_points={
         'console_scripts': [
-            'regulator = labgrid.__main__:main',
+            'regulator = regulator.__main__:main',
         ]
     },
 )
