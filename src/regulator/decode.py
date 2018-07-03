jluebbe/regulator

commit 92e95951583f5ce0bf20d787cbaa097ca6134291

decode: be more verbose when logic errors happen
Show the user a slight glimpse where errors could be hidden.

Signed-off-by: Roland Hieber <r.hieber@pengutronix.de>

src/regulator/decode.py
@@ -53,22 +53,26 @@ class Type:
     enums = attr.ib(default=None)
 
     def __attrs_post_init__(self):
-        self.kind = Kind.from_str(self.kind)
-        fields = SortedListWithKey(key=lambda x: x.location)
-        for k, v in self.fields.items():
-            kind, location = k.split()
-            if isinstance(v, str):
-                name = v
-                config = {}
-            else:
-                name = v.pop('name')
-                config = v
-            if "enum" in config.keys() and isinstance(config["enum"], str):
-                assert config["enum"] in self.enums.keys()
-                config["enum"] = self.enums[config["enum"]]
-            field = Field(name, kind, location, **config)
-            fields.add(field)
-        self.fields = fields
+        try:
+            self.kind = Kind.from_str(self.kind)
+            fields = SortedListWithKey(key=lambda x: x.location)
+            for k, v in self.fields.items():
+                kind, location = k.split()
+                if isinstance(v, str):
+                    name = v
+                    config = {}
+                else:
+                    name = v.pop('name')
+                    config = v
+                if "enum" in config.keys() and isinstance(config["enum"], str):
+                    assert config["enum"] in self.enums.keys()
+                    config["enum"] = self.enums[config["enum"]]
+                field = Field(name, kind, location, **config)
+                fields.add(field)
+            self.fields = fields
+        except:
+            print("In type {}:".format(self.name))
+            raise
 
     def __len__(self):
         return len(self.kind)

commit 5d99205d1b093bec5b8458e1cc403925bd448c31

decode: re-do error reporting
Commit 92e9595 was okay, but we can improve on it:

- also write the hint to stderr where the exception is printed
- also report errors in clusters and instances
- improve and declutter the hint message

Fixes: commit 92e95951583f5ce0bf20d787cbaa097ca6134291 ("decode: be more
  verbose when logic errors happen")
Signed-off-by: Roland Hieber <rohieb@rohieb.name>

src/regulator/decode.py
@@ -1,5 +1,6 @@
 import attr
 import bitstruct
+import sys
 import yaml
 from prettyprinter import pprint
 from sortedcontainers import SortedListWithKey
@@ -56,18 +57,22 @@ class Type:
         self.kind = Kind.from_str(self.kind)
         fields = SortedListWithKey(key=lambda x: x.location)
         for k, v in self.fields.items():
-            kind, location = k.split()
-            if isinstance(v, str):
-                name = v
-                config = {}
-            else:
-                name = v.pop('name')
-                config = v
-            if "enum" in config.keys() and isinstance(config["enum"], str):
-                assert config["enum"] in self.enums.keys()
-                config["enum"] = self.enums[config["enum"]]
-            field = Field(name, kind, location, **config)
-            fields.add(field)
+            try:
+                kind, location = k.split()
+                if isinstance(v, str):
+                    name = v
+                    config = {}
+                else:
+                    name = v.pop('name')
+                    config = v
+                if "enum" in config.keys() and isinstance(config["enum"], str):
+                    assert config["enum"] in self.enums.keys()
+                    config["enum"] = self.enums[config["enum"]]
+                field = Field(name, kind, location, **config)
+                fields.add(field)
+            except:
+                sys.stderr.write("Note: in field '{}':\n".format(k))
+                raise
         self.fields = fields
 
     def __len__(self):
@@ -104,22 +109,30 @@ class Cluster:
     def __attrs_post_init__(self):
         types = {}
         for k, v in self.types.items():
-            kind, name = k.split()
-            assert kind in ['r32']
-            types[name] = Type(name, kind, **v)
+            try:
+                kind, name = k.split()
+                assert kind in ['r32']
+                types[name] = Type(name, kind, **v)
+            except:
+                sys.stderr.write("Note: in type '{}':\n".format(k))
+                raise
         self.types = types
 
         registers = SortedListWithKey(key=lambda x: x.location)
         for k, v in self.registers.items():
-            kind, location = k.split(' ', 1)
-            if isinstance(v, str):
-                name = v
-                config = {}
-            else:
-                name = v.pop('name')
-                config = v
-            register = Register(name, kind, location, config.get('type'))
-            registers.add(register)
+            try:
+                kind, location = k.split(' ', 1)
+                if isinstance(v, str):
+                    name = v
+                    config = {}
+                else:
+                    name = v.pop('name')
+                    config = v
+                register = Register(name, kind, location, config.get('type'))
+                registers.add(register)
+            except:
+                sys.stderr.write("Note: in register '{}':\n".format(k))
+                raise
         self.registers = registers
 
     @property
@@ -165,18 +178,26 @@ class Decoder:
 
         clusters = {}
         for name, config in layout['clusters'].items():
-            cluster = Cluster(name, **config)
-            clusters[name] = cluster
+            try:
+                cluster = Cluster(name, **config)
+                clusters[name] = cluster
+            except:
+                sys.stderr.write("Note: in cluster '{}':\n".format(name))
+                raise
 
         instances = {}
         for k, v in layout['instances'].items():
-            cluster_name, start = k.split(' ', 1)
-            start = int(start, 0)
-            name = v
-            cluster = clusters[cluster_name]
-            location = Location(start, start+cluster.size)
-            instance = Instance(name, cluster_name, location)
-            instances[name] = instance
+            try:
+                cluster_name, start = k.split(' ', 1)
+                start = int(start, 0)
+                name = v
+                cluster = clusters[cluster_name]
+                location = Location(start, start+cluster.size)
+                instance = Instance(name, cluster_name, location)
+                instances[name] = instance
+            except:
+                sys.stderr.write("Note: in instance '{}':\n".format(k))
+                raise
 
         self.layout = layout
         self.clusters = clusters